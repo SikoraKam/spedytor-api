3b0a4ced44b58846ba3768e193b31a3b4d7d24f2
SikoraKam <sikorakam3@gmail.com>
2021-08-24 14:28:56 +0200

    auth initialization, login, register

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import * as jwt from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findUserByName(username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password: pass, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any): Promise<any> {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(data: any): Promise<any> {
+    const user = await this.usersService.register(data);
+    return user;
+  }
+}
