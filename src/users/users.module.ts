0b74720e16340d3c52130c9bbce009b014519155
SikoraKam <sikorakam3@gmail.com>
2021-09-03 15:55:36 +0200

    added connection with database

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,13 @@
 import { UsersService } from './users.service';
 import { Module } from '@nestjs/common';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UsersService],
   exports: [UsersService],
 })
